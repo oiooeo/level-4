674a967cdaba05d1bbba432a4adf80a40635f816
Choi Yoonseo <choiys1103@naver.com>
2023-07-12 15:54:19 +0900

    feat. 삭제 확인창

---
+++ src/components/Modal/DeleteConfirm.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Modal from "./Modal";
+import { styled } from "styled-components";
+import Button from "../Button";
+import api from "../../axios/api";
+import { useNavigate } from "react-router-dom";
+
+function DeleteConfirm({ id, isModalOpen, setIsModalOpen }) {
+  const navigate = useNavigate();
+
+  const deleteButtonClickHandler = () => {
+    api.delete(`polaroid/${id}`);
+    navigate("/");
+  };
+
+  const cancelButtonClickHandler = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+      <Type>⚠️ Warning !</Type>
+      <Text>게시글을 삭제하시겠습니까?</Text>
+
+      <Buttons>
+        <Button
+          size={"small"}
+          color={"#f89b9b"}
+          onClick={deleteButtonClickHandler}
+        >
+          삭제
+        </Button>
+
+        <Button
+          size={"small"}
+          color={"#9e9e9e"}
+          onClick={cancelButtonClickHandler}
+        >
+          취소
+        </Button>
+      </Buttons>
+    </Modal>
+  );
+}
+
+export default DeleteConfirm;
+
+const Type = styled.p`
+  font-weight: 500;
+`;
+
+const Text = styled.p`
+  margin: 20px;
+  font-weight: 400;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+`;
